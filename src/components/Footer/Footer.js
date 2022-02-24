fb4fcd76e26bab8753f5b593c04bb1b178abff51
TUSHAR CHAUHAN <91833740+Tushar-chauhan198@users.noreply.github.com>
2022-02-24 17:47:53 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => {
             </LinkColumn>
            <LinkColumn>
           <LinkTitle>✉️ Location</LinkTitle>
-          <LinkItem href="Hapur">Hapur,Pilkhuwa</LinkItem>
+          <LinkItem href="Hapur">Pilkhuwa, Hapur</LinkItem>
         
         </LinkColumn>
       </LinkList>
